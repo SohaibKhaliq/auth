7baa6ea2e59b2515c01ab0a527e97e07290050fc
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-03 12:51:52 +0500

    Implement user registration and email verification functionality in index.php

--- src/index.php
+++ src/index.php
@@ -1,11 +1,75 @@
+<?php
+require_once '../vendor/autoload.php';
+//uacs pcnh zfdu jwzj
+use PHPMailer\PHPMailer\PHPMailer;
+
+$connection = new mysqli('localhost', 'root', '201734', 'auth', 3306);
+if ($connection->connect_error) {
+    echo "there is an error in connecting to database";
+    die();
+}
+echo "database is connected";
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    $fullname = $_POST['fullname'];
+    $password = md5($_POST['password']);
+    $email = $_POST['email'];
+
+    $query="select email from users where email='$email' ";
+    $res=mysqli_query($connection,$query);
+    $num=mysqli_num_rows($res);
+    if($num>0){
+        echo "<script>alert('Email already registered')</script>";
+        exit();
+        header("Location: index.php");
+    }
+    $token=bin2hex(random_bytes(8));
+
+    // print $name . '</br>' .$cnic . '</br>' .$password . '</br>' .$city . '</br>';
+    $sql = "insert into  users(name,password,email,token)values ('$fullname','$password','$email','$token') ";
+    $result = mysqli_query($connection, $sql);
+    if ($result == true) {
+        echo "<script>alert('data is inserted')</script>";
+        // header("Location: record.php");
+    } else {
+        echo "<script>alert('data is not inserted')</script>";
+    }
+
+    $mail=new PHPMailer();
+    try{
+        $mail->isSMTP();
+        $mail->Host="smtp.gmail.com";
+        $mail->SMTPAuth=true;
+        $mail->Username="sohaibkhaliq510@gmail.com";
+        $mail->Password="uacs pcnh zfdu jwzj";
+        $mail->SMTPSecure="tls";
+        $mail->Port=465;
+        $mail->setFrom("sohaibkhaliq510@gmail.com");
+        $mail->addAddress($email);
+        $mail->isHTML(true);
+        $mail->Subject="Verify your email";
+        $mail->Body="Click the link to verify your email <a href='http://localhost/auth/src/verify.php?token=$token'>Verify Email</a>";
+        $mail->send();
+        echo "<script>alert('Verification link has been sent to your email')</script>";
+    }
+    catch(Exception $e){
+        echo "<script>alert('Error: {$e->getMessage()}')</script>";
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <title>login page</title>
 </head>
 
 <body>
@@ -28,13 +92,13 @@
             <div class="user_options-forms" id="user_options-forms">
                 <div class="user_forms-login">
                     <h2 class="forms_title">Login</h2>
-                    <form class="forms_form">
+                    <form class="forms_form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" class="register-form">>
                         <fieldset class="forms_fieldset">
                             <div class="forms_field">
-                                <input type="email" name="email" placeholder="Email" class="forms_field-input" required autofocus />
+                                <input type="email" placeholder="Email" class="forms_field-input" name="email" required autofocus />
                             </div>
                             <div class="forms_field">
-                                <input type="password" name="password" placeholder="Password" class="forms_field-input" required />
+                                <input type="password" placeholder="Password" class="forms_field-input" name="password" required />
                             </div>
                         </fieldset>
                         <div class="forms_buttons">
@@ -45,10 +109,10 @@
                 </div>
                 <div class="user_forms-signup">
                     <h2 class="forms_title">Sign Up</h2>
-                    <form class="forms_form">
+                    <form class="forms_form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" class="register-form"> >
                         <fieldset class="forms_fieldset">
                             <div class="forms_field">
-                                <input type="text" placeholder="Full Name" class="forms_field-input" name="name" required />
+                                <input type="text" placeholder="Full Name" class="forms_field-input" name="fullname" required />
                             </div>
                             <div class="forms_field">
                                 <input type="email" placeholder="Email" class="forms_field-input" name="email" required />
@@ -66,7 +130,7 @@
         </div>
     </section>
 
-
 </body>
 <script src="script.js"></script>
+
 </html>
